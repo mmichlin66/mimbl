74968fb6c3b34a3a97c98bbccd7c68173900f07f
Michael Michlin <mmulya@outlook.com>
2020-02-14 23:53:25 -0500

    Remove cssSelector file

--- src/mimblTypes.ts
+++ src/mimblTypes.ts
@@ -4,5 +4,4 @@ export * from "./api/mim";
 export * from "./api/HtmlTypes";
 export * from "./api/SvgTypes";
 export * from "./api/LocalStyles";
-export * from "./api/cssSelector";
 export * from "./utils/EventSlot";
