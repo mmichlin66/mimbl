626012b25dbd56b75d51d582f511d0e13eb119f1
Michael Michlin <mmulya@outlook.com>
2020-08-11 15:17:48 -0400

    Typedoc related changes.

--- doc.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-typedoc --out ./reference src/api/mim.ts src/api/HtmlTypes.ts src/api/SvgTypes.ts src/api/LocalStyles.ts --readme none --excludeExternals --excludeNotExported --excludePrivate
